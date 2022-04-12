c3dce44987e9f84ce675211d5a98723eb7561252
Kerem Okyay <82160210+BayKeremm@users.noreply.github.com>
2022-04-12 20:32:55 +0200

    matched features not bad

--- frameHelper.py
+++ frameHelper.py
@@ -10,7 +10,7 @@ class Frame(object):
         orb = cv2.ORB_create()
         #detection
         #TODO: Understand the function below
-        feats = cv2.goodFeaturesToTrack(frame,1000,qualityLevel=0.01,minDistance = 1)
+        feats = cv2.goodFeaturesToTrack(frame,3000,qualityLevel=0.01,minDistance = 1)
         #extraction
         kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in feats]
 
@@ -20,12 +20,34 @@ class Frame(object):
         #getters
         @property
         def data(self):
-            return sef.data
+            return self.data
         @property
         def kps(self):
-            return sef.kps
+            return self.kps
         @property
         def des(self):
-            return sef.des
-
+            return self.des
 #TODO: Add frame manager
+class FrameManager(object):
+    def __init__(self):
+        self._frames = []
+    def addFrame(self,frame):
+        self.frames.append(frame)
+    @property
+    def frames(self):
+        return self._frames
+    
+    def matchFrames(self,f1,f2):
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+        matches = bf.knnMatch(f1.des,f2.des,k=2)
+
+        #ratio test
+        good = []
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                p1=f1.kps[m.queryIdx].pt
+                p2=f2.kps[m.trainIdx].pt
+                good.append([p1, p2])
+        good = np.asarray(good,dtype=int)
+        return good
+
--- slam.py
+++ slam.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-from frameHelper import Frame
-
+from frameHelper import Frame,FrameManager
 cap = cv2.VideoCapture("test_countryroad.mp4")
+frmManager = FrameManager();
 while True:
     ret,frame = cap.read()
     if ret is False:
@@ -10,13 +10,18 @@ while True:
     
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_gray = cv2.cvtColor(frame_rgb,cv2.COLOR_RGB2GRAY)
-
     frame_output = np.copy(frame_rgb)
-    frm = Frame(frame_gray)
-    kps = frm.kps
-    des = frm.des
 
-    cv2.drawKeypoints(frame,kps,frame_output,color = (0,255,0))
+
+    frm = Frame(frame_gray)
+    frmManager.addFrame(frm)
+    if len(frmManager.frames)==1:
+        continue
+    matches = frmManager.matchFrames(frmManager.frames[-1],frmManager.frames[-2])
+
+    for p1,p2 in matches:
+        cv2.line(frame_output, tuple(p1), tuple(p2), (0, 255, 0))
+        cv2.circle(frame_output, tuple(p2), 3, (0, 255, 0))
 
     cv2.imshow("Frame",frame_output)
     if cv2.waitKey(1) & 0xFF == ord('q'):
